835be37d5e33de1f2f4ccfdef8c87d9ea08c0205
IvanGM <ivangm_01@hotmail.com>
2024-02-27 21:35:12 -0600

    feat: Archivos de clase 1 añadidos

--- tareas-front/src/App.jsx
+++ tareas-front/src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Components/Header'
+import Dashboard from './Pages/Dashboard'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+
 const App = () => {
   return (
-    <h1>Hola Mundo</h1>
+    <Router>
+      <div className='container'>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
